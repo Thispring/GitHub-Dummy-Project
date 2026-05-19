16a6b6fe407b7a01a1f73c2a1f1d9843e41ff43d
Thispring <kisuk578@naver.com>
2026-05-19 13:57:30 +0900

    점프 비활성화

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
-        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        //if (Input.GetKeyDown(KeyCode.Space)) Jump();
         if (Input.GetKeyDown(KeyCode.P)) SpawnProjectile();
     }
 
